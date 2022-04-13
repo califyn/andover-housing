cd23a3540d75cd49eb8cd364b2665bbda166d15b
Ali Cy <califynic@gmail.com>
2022-04-13 17:11:37 -0400

    semi-final version of yearbooks

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var splash_id = 1;
 var internal_count = 0;
 function toggleSplash() {
-    console.log('toggle');
     internal_count++;
     if (internal_count % 6 == 0) {
         splash_id = 2;
@@ -32,57 +31,53 @@ var dorms = {
     'WQS': wqs
 };
 var dorm_avail = `
-00000000000000001011011111111100001010111
-00100000000000110000111111111110001010111
-00110000000000100010111111111110001000111
-00000000000000000001011111111110001000111
-00000000000000000011011111101110001010111
-00110000000000000001011111111110001010111
-00110000000000000010111111111110001010111
-00110000000000000001111111111110001010111
-00010000000000000011011111111110001010111
-00110000000000110001111111111110001010111
-00000000000000100011011011111110001010111
-00000000000000000011011111111110001010111
-00010000000000000011111111011110001010111
-00110000000000000000111111111100001010111
-00000000000000000001111111111110001010111
-00110000000000000011111111111110001010111
-00000000000000010000111111111110001010111
-00110000000000010000111111111110001010111
-00100000000000001001111111111110001010111
-00000000000000000001111111111110001010011
-00110000000000000000111111111110000000011
-00010000000000000011111111111110001010111
-00010000000000010010111111111110001010111
-00110000000000000001111111111110001010111
-00110000000000000011011111111110001010111
-00010000000000100011111111111110001010111
-00010000000000000000111111111110001010111
-00010000000000000011111111111110001010111
-00110000000000100010111111111110001000111
-00010000000000101010111111111100001010111
-00110000000000001011111111111110001010111
-00110000000000000011111111111110001010111
-00000000000000001010111111111110001010111
-00010000000000001000111111111110001010111
-00000000000000000010111111111110001010111
-00000000000000001001111111111110001010111
-00010000000000010011011111111110001010111
-00010000000000100011111111111110001010111
-00110000000000000011111111111110001010111
-00000000000000000011111111111100001010111
-00000000000000000010111111111110001010111`; // dorm x year 1 if available, 0 if not (THANKS I HATE THIS)
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,0,1,2,1,1,1,1,1,1,1,0,0,0,0,1,0,1,0,1,1,1
+0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,0,1,0,1,1,1
+0,0,1,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,0,0,0,1,1,1
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,1,1,3,0,0,0,1,0,0,0,1,1,1
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,1,1,0,1,1,1,0,0,0,1,0,1,0,1,1,1
+0,0,3,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,1,1,5,0,0,0,1,0,1,0,1,1,1
+0,0,2,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,0,1,0,1,1,1
+0,0,3,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,0,1,0,1,1,1
+0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,1,1,1,1,1,5,0,0,0,1,0,1,0,1,1,1
+0,0,2,2,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,0,1,0,1,1,1
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,1,0,1,1,0,1,1,1,1,1,1,8,0,0,0,1,0,1,0,1,1,1
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,1,1,1,1,1,8,0,0,0,1,0,1,0,1,1,1
+0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,0,2,2,2,12,0,0,0,1,0,1,0,1,1,1
+0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,0,1,0,1,1,1
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,0,1,0,1,1,1
+0,0,1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,2,2,2,2,2,0,0,0,1,0,1,0,1,1,1
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,1,1,1,1,1,2,1,1,1,5,0,0,0,1,0,1,0,1,1,1
+0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,2,2,2,2,2,2,2,2,2,2,2,0,0,0,1,0,1,0,1,1,1
+0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,2,1,2,2,1,2,2,1,2,2,2,4,0,0,0,1,0,1,0,1,1,1
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,1,1,1,1,2,1,1,1,1,1,6,0,0,0,1,0,1,0,0,1,1
+0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,9,0,0,0,0,0,0,0,0,1,1
+0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,1,2,2,2,2,0,0,0,1,0,1,0,1,1,1
+0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,1,1,1,1,1,1,1,1,1,1,8,0,0,0,1,0,1,0,1,1,1
+0,0,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,1,1,2,1,3,0,0,0,1,0,1,0,1,1,1
+0,0,2,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,0,2,2,2,2,2,2,2,2,2,21,0,0,0,1,0,1,0,2,2,1
+0,0,0,1,0,0,0,0,0,0,0,0,0,0,2,0,0,0,2,2,2,2,2,2,2,2,2,3,2,2,2,0,0,0,1,0,1,0,1,1,1
+0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,2,2,2,0,0,0,1,0,1,0,1,1,1
+0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,1,2,2,2,2,2,2,3,0,0,0,1,0,1,0,1,1,1
+0,0,1,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,0,0,0,1,1,1
+0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,1,1,1,1,1,1,1,1,2,0,0,0,0,1,0,1,0,1,1,1
+0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,2,0,2,2,1,2,2,2,2,2,2,2,2,2,13,0,0,0,1,0,1,0,1,1,1
+0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,2,0,0,0,1,0,2,0,1,1,1
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,1,1,1,1,1,1,1,1,1,3,0,0,0,1,0,1,0,1,1,1
+0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,1,1,1,1,1,1,1,1,1,14,0,0,0,1,0,1,0,1,1,1
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,1,1,1,2,0,0,0,1,0,1,0,1,1,1
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,2,2,2,2,2,2,2,2,2,2,2,2,0,0,0,2,0,2,0,2,2,2
+0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,1,0,0,2,2,0,2,2,2,2,2,2,2,2,2,10,0,0,0,1,0,1,0,1,1,1
+0,0,0,2,0,0,0,0,0,0,0,0,0,0,1,0,0,0,2,2,2,2,2,2,2,2,2,2,2,2,19,0,0,0,1,0,1,0,1,1,1
+0,0,1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,1,1,1,1,1,2,2,2,2,0,0,0,1,0,1,0,1,1,1
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,0,1,0,1,1,1
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,1,1,1,2,0,0,0,1,0,1,0,1,1,1
+`; // dorm x year 1 if available, 0 if not (THANKS I HATE THIS)
 dorm_avail = dorm_avail.split("\n").slice(1);
-console.log(dorm_avail);
 for (var i = 0; i < dorm_avail.length; i++) {
-    ret = [];
-    for (var j = 0; j < dorm_avail[i].length; j++) {
-        if (dorm_avail[i].charAt(j) == '0') {
-            ret.push(false);
-        } else {
-            ret.push(true);
-        }
+    ret = dorm_avail[i].split(",");
+    for (var j = 0; j < ret.length; j++) {
+        ret[j] = parseInt(ret[j]); 
     }
     dorm_avail[i] = ret;
 }
@@ -91,7 +86,7 @@ function drawTTY() {
     const svg = d3.select('#tty-svg');
     svg.selectAll("*").remove();
     var focused_dorm = 'Alumni';
-    var focused_years = [1989, 1994, 1999, 2007, 2012];
+    var focused_years = [1989, 1994, 1999, 2002, 2012];
     var width = svg.node().getBoundingClientRect().width;
     var height = svg.node().getBoundingClientRect().height;
     var clusters = Object.keys(dorms);
@@ -111,6 +106,16 @@ function drawTTY() {
         for (var i = 0; i < dorm_texts.length; i++) {
             dorm_texts[i].attr("font-weight", "400");
             dorm_texts[i].attr("fill", dorm_gray);
+            dorm_texts[i].on('mouseover', function () {
+                    if (focused_dorm != d3.select(this).text()) {
+                        d3.select(this).attr('text-decoration', 'underline');
+                    }
+                    document.body.style.cursor = "pointer";
+                })
+                .on('mouseout', function () {
+                    d3.select(this).attr('text-decoration', 'none');
+                    document.body.style.cursor = "default";
+                });
         }
         elem.attr("font-weight", "700");
         elem.attr('text-decoration', 'none');
@@ -148,14 +153,23 @@ function drawTTY() {
                     if (focused_dorm != d3.select(this).text()) {
                         d3.select(this).attr('text-decoration', 'underline');
                     }
+                    document.body.style.cursor = "pointer";
                 })
                 .on('mouseout', function () {
                     d3.select(this).attr('text-decoration', 'none');
+                    document.body.style.cursor = "default";
                 })
                 .on('click', function () {
                     setDorm(d3.select(this));
                 })
             );
+            if (dorms[clusters[i]][j] == focused_dorm) {
+                dorm_texts[dorm_texts.length - 1]
+                    .attr("fill", "black")
+                    .attr("font-weight", '700')
+                    .on('mouseover', function () { })
+                    .on('mouseout', function () { });
+            }
             previousY += dorm_height;
         }
 
@@ -173,16 +187,17 @@ function drawTTY() {
     var circle_x = 50;
     var vspacing = 21.45;
     var image_w = 350;
-    var image_h = 165;
+    var image_h = 155;
     var image_x = 150 + image_w / 2;
 
     for (var i = 1973; i < 2014; i++) {
         var thisyear = {};
-        var is_available = dorm_avail[dorm_raws.indexOf(focused_dorm)][i - 1973];
+        var is_available = dorm_avail[dorm_raws.indexOf(focused_dorm)][i - 1973] > 0;
         thisyear.innercirc = svg.append("circle")
             .attr("r", "2")
             .attr("cx", circle_x)
-            .attr("cy", previousY);
+            .attr("cy", previousY)
+            .attr("fill", "#888");
         thisyear.outercirc = svg.append("circle")
             .attr("fill", "transparent")
             .style("stroke", "white")
@@ -195,13 +210,26 @@ function drawTTY() {
             .text(i + '')
             .attr('x', circle_x - 10)
             .attr('y', previousY + 4)
-            .attr("fill", "#000");
+            .attr("fill", "#888");
         if (! is_available) {
             thisyear.innercirc.attr("fill", "#ddd");
             thisyear.text.attr("fill", "#ddd");
+        } else {
+            //thisyear.innercirc.attr("fill", "#888");
+            thisyear.text.on('mouseover', function() {
+                addYear(d3.select(this));
+            });
         }
+
         if (focused_years.includes(i)) {
-            thisyear.outercirc.style("stroke", "black");
+            console.log(i);
+            console.log('original');
+            //thisyear.outercirc.style("stroke", "black");
+            //thisyear.innercirc.style("fill", "black");
+            thisyear.text.attr('font-weight', '700');
+            thisyear.text.attr('fill', 'black');
+        } else {
+            thisyear.text.attr('font-weight', '400');
         }
 
         previousY += vspacing;
@@ -215,9 +243,53 @@ function drawTTY() {
         shuffle_pos.push(0);
     }
 
+    var defs = svg.append("defs");
+    var back1 = defs.append('filter')
+        .attr('id', 'back1')
+    back1.append('feColorMatrix')
+        .attr('type', 'matrix')
+        .attr('values', '0 0 0 0 0.4 0 0 0 0 0.4 0 0 0 0 0.4 0 0 0 0.8 0')
+        .attr("result", "gray");
+    back1.append("feGaussianBlur")
+        .attr('in', 'SourceGraphic')
+        .attr("stdDeviation","0.1")
+        .attr("result","blur");
+    var merge = back1.append('feMerge');
+    merge.append('feMergeNode')
+        .attr('in', 'blur');
+    merge.append('feMergeNode')
+        .attr('in', 'gray');
+    var back1 = defs.append('filter')
+        .attr('id', 'back2')
+    back1.append('feColorMatrix')
+        .attr('type', 'matrix')
+        .attr('values', '0 0 0 0 0.8 0 0 0 0 0.8 0 0 0 0 0.8 0 0 0 0.9 0')
+        .attr("result", "gray");
+    back1.append("feGaussianBlur")
+        .attr('in', 'SourceGraphic')
+        .attr("stdDeviation","0.1")
+        .attr("result","blur");
+    var merge = back1.append('feMerge');
+    merge.append('feMergeNode')
+        .attr('in', 'blur');
+    merge.append('feMergeNode')
+        .attr('in', 'gray');
+
+    function shuffleImg(elem) {
+        var idx = parseInt(elem.attr('class').split(' ')[1].split("-")[2]);
+        shuffle_pos[idx]++;
+        if (shuffle_pos[idx] == dorm_avail[dorm_raws.indexOf(focused_dorm)][focused_years[idx] - 1973]) {
+            shuffle_pos[idx] = 0;
+        }
+        svg.selectAll(".tty-images-" + idx)
+            .attr("xlink:href", "https://raw.githubusercontent.com/califynic/andover-housing/main/assets/yearbooks/" + focused_years[idx] + "/" + focused_dorm + "/" + shuffle_pos[idx] + ".png")
+    }
+
+    var imageYs = [];
     function updateImages() {
         previousY = tab_top + image_h/2;
         svg.selectAll(".tty-images").remove();
+        imageYs = [];
         var new_pairs = [];
         for (var i = 0; i < focused_years.length; i++) {
             new_pairs.push([focused_dorm, focused_years[i]]);
@@ -230,8 +302,30 @@ function drawTTY() {
         cached_pairs = new_pairs;
 
         for (var i = 0; i < focused_years.length; i++) {
-            console.log(focused_years[i]);
-            console.log(focused_dorm);
+            imageYs.push(previousY);
+            var num_pics = dorm_avail[dorm_raws.indexOf(focused_dorm)][focused_years[i] - 1973];
+            if (num_pics > 2) {
+                svg.append("svg:image")
+                    .attr("class", "tty-images back-images tty-images-" + i)
+                    .attr("x", image_x + 8)
+                    .attr("y", previousY + 15)
+                    .attr("width", image_w)
+                    .attr("height", image_h)
+                    .attr("transform", "translate(" + (-image_w/2) + ", " + (-image_h/2) + ") rotate(-0.4)")
+                    .attr("xlink:href", "https://raw.githubusercontent.com/califynic/andover-housing/main/assets/yearbooks/" + focused_years[i] + "/" + focused_dorm + "/" + shuffle_pos[i] + ".png")
+                    .attr("filter", "url(#back2)");
+            }
+            if (num_pics > 1) {
+                svg.append("svg:image")
+                    .attr("class", "tty-images back-images tty-images-" + i)
+                    .attr("x", image_x + 4)
+                    .attr("y", previousY + 7.5)
+                    .attr("width", image_w)
+                    .attr("height", image_h)
+                    .attr("transform", "translate(" + (-image_w/2) + ", " + (-image_h/2) + ") rotate(-0.2)")
+                    .attr("xlink:href", "https://raw.githubusercontent.com/califynic/andover-housing/main/assets/yearbooks/" + focused_years[i] + "/" + focused_dorm + "/" + shuffle_pos[i] + ".png")
+                    .attr("filter", "url(#back1)");
+            }
             svg.append("svg:image")
                 .attr("class", "tty-images tty-images-" + i)
                 .attr("x", image_x)
@@ -239,8 +333,16 @@ function drawTTY() {
                 .attr("width", image_w)
                 .attr("height", image_h)
                 .attr("transform", "translate(" + (-image_w/2) + ", " + (-image_h/2) + ")")
-                .attr("xlink:href", "https://raw.githubusercontent.com/califynic/andover-housing/main/assets/yearbooks/" + focused_years[i] + "/" + focused_dorm + "/0.png");        
-            previousY += image_h + 10;
+                .attr("xlink:href", "https://raw.githubusercontent.com/califynic/andover-housing/main/assets/yearbooks/" + focused_years[i] + "/" + focused_dorm + "/" + shuffle_pos[i] + ".png");        
+            svg.selectAll(".tty-images-" + i)
+                .on('click', function () {
+                    shuffleImg(d3.select(this));
+                }).on('mouseover', function () {
+                    document.body.style.cursor = 'pointer';
+                }).on('mouseout', function () {
+                    document.body.style.cursor = 'default';
+            });
+            previousY += image_h + 20;
         }
     }
 
@@ -248,7 +350,7 @@ function drawTTY() {
         title.text(focused_dorm);
         var available_years = [];
         for (var i = 1973; i < 2014; i++) {
-            if (dorm_avail[dorm_raws.indexOf(focused_dorm)][i - 1973]) {
+            if (dorm_avail[dorm_raws.indexOf(focused_dorm)][i - 1973] > 0) {
                 available_years.push(i);
             }
         }
@@ -260,27 +362,76 @@ function drawTTY() {
                         options.push(available_years[j]);
                     }
                 }
-                console.log(options);
                 focused_years[i] = options[getIndex(options, focused_years[i])];
             }
         } 
         for (var i = 1973; i < 2014; i++) {
-            var is_available = dorm_avail[dorm_raws.indexOf(focused_dorm)][i - 1973];
+            var is_available = dorm_avail[dorm_raws.indexOf(focused_dorm)][i - 1973] > 0;
             if (is_available) {
-                years[i + ''].innercirc.attr("fill", "black");
-                years[i + ''].text.attr("fill", "black");
+                years[i + ''].innercirc.attr("fill", "#888");
+                years[i + ''].text.attr("fill", "#888");
+                years[i + ''].text.on('mouseover', function() {
+                    addYear(d3.select(this));
+                });
             } else {
                 years[i + ''].innercirc.attr("fill", "#ddd");
                 years[i + ''].text.attr("fill", "#ddd");
+                years[i + ''].text.on('mouseover', function () {});
             }
             years[i + ''].outercirc.style("stroke", "white");
             if (focused_years.includes(i)) {
+                years[i + ''].innercirc.attr("fill", "black");
                 years[i + ''].outercirc.style("stroke", "black");
+                years[i + ''].text.attr('font-weight', '700');
+                years[i + ''].text.attr('fill', 'black');
+            } else {
+                years[i + ''].text.attr('font-weight', '400');
             }
         }
-        console.log('history')
+        if (typeof connectors !== 'undefined') {
+            for (var i = 0; i < focused_years.length; i++) {
+                var path = d3.path();
+                var x1 = circle_x + 25;
+                var y1 = years[focused_years[i] + ''].y
+                var x2 = image_x - image_w / 2;
+                var y2 = imageYs[i];
+                path.moveTo(x1, y1);
+                path.bezierCurveTo(x2, y1, x1, y2, x2, y2);
+                path.lineTo(image_x, y2);
+                connectors[i].attr("d", path);
+            }
+        }
         updateImages();
     }
+
+    function addYear(elem) {
+        var yr = parseInt(elem.text());
+        var dists = [];
+        for (var i = 0; i < focused_years.length; i++) {
+            dists.push((focused_years[i] - yr) ** 2);
+        }
+        focused_years[dists.indexOf(Math.min(...dists))] = yr;
+        setHistoryPanel();
+    }
+    setHistoryPanel();
+    var connectors = [];
+    for (var i = 0; i < focused_years.length; i++) {
+        var path = d3.path();
+        var x1 = circle_x + 25;
+        var y1 = years[focused_years[i] + ''].y
+        var x2 = image_x - image_w / 2;
+        var y2 = imageYs[i];
+        path.moveTo(x1, y1);
+        path.bezierCurveTo(x2, y1, x1, y2, x2, y2);
+        path.lineTo(image_x, y2);
+        connectors.push(svg.append("path")
+            .style("stroke", "black")
+            .attr("stroke-width", "1.5px")
+            .style("fill", "none")
+            .attr("d", path)
+            .lower());
+    }
+    console.log(connectors);
 }
 
 window.onload = drawTTY;
